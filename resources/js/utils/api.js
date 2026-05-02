31b1ce7f9babab7966d68bccffc89c7bbc0a70a7
Vineet Pandey <vineetpandey051996@gmail.com>
2026-05-02 14:36:51 +0530

    changes

--- resources/js/utils/api.js
+++ resources/js/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: window.APP_URL ? `${window.APP_URL}/api` : '/api',
+  baseURL: window.APP_URL ? `${window.APP_URL.replace(/\/$/, '')}/api` : '/api',
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
